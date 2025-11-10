72076ebe802d1e3e0071bd465fec60d82cc5e90f
zuhra <zuhraaldaiarova@icloud.com>
2025-11-10 22:35:12 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const click1=()=>{
+   let div= document.getElementById("block")
+   div.style.width="500px"
+   div.style.height="180px"
+    div.style.backgroundColor="green"
+    div.style.borderRadius="30px"
+}
+
+
+const click2=()=>{
+   let div= document.getElementById("block2")
+   div.style.width="200px"
+   div.style.height="200px"
+    div.style.backgroundColor="blue"
+    div.style.borderRadius="50%"
+}
+
+
+const click3=()=>{
+   let div= document.getElementById("block3")
+   div.style.width="200px"
+   div.style.height="250px"
+    div.style.backgroundColor="pink"
+    div.style.borderRadius="40px"
+}
